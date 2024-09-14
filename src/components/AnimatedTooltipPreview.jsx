e45f1962a672637cd35e2d3b19cef42de89f0cbd
s-kvng <kvngnathan8420@gmail.com>
2024-09-14 02:17:12 +0000

    ui: add member images :information_desk_person:

--- src/components/AnimatedTooltipPreview.jsx
+++ src/components/AnimatedTooltipPreview.jsx
@@ -1,56 +1,57 @@
 "use client";
 import React from "react";
-import Image from "next/image";
+
 import { AnimatedTooltip } from "../components/ui/animated-tooltip";
+import nath from "../public/members/CTO_Nathan.jpg";
+import kobby from "../public/members/CEO-Kobby.jpg";
+import kwame from "../public/members/COO-Emma.jpg";
+
 const people = [
   {
     id: 1,
     name: "Andrew Nana Beniako",
     designation: "Chief Executive Officer",
-    Image:
-      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    image: kobby,
   },
   {
     id: 2,
     name: "Nathaniel Edem Adama",
     designation: "Chief Technical Officer",
-    Image:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    image: nath,
   },
   {
     id: 3,
     name: "Emmanuel Acquah",
     designation: "Chief Operations Officer",
-    Image:
-      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    image: kwame,
   },
   {
     id: 4,
     name: "Charity Boateng",
     designation: "Chief Financial Officer",
-    Image:
+    image:
       "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
   },
-//   {
-//     id: 5,
-//     name: "Tyler Durden",
-//     designation: "Soap Developer",
-//     Image:
-//       "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
-//   },
-//   {
-//     id: 6,
-//     name: "Dora",
-//     designation: "The Explorer",
-//     Image:
-//       "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
-//   },
+  //   {
+  //     id: 5,
+  //     name: "Tyler Durden",
+  //     designation: "Soap Developer",
+  //     Image:
+  //       "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+  //   },
+  //   {
+  //     id: 6,
+  //     name: "Dora",
+  //     designation: "The Explorer",
+  //     Image:
+  //       "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+  //   },
 ];
 
 export function AnimatedTooltipPreview() {
   return (
-    (<div className="flex flex-row items-center justify-center mb-10 w-full">
+    <div className="flex flex-row items-center justify-center mb-10 w-full">
       <AnimatedTooltip items={people} />
-    </div>)
+    </div>
   );
 }
